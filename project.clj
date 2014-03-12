6ed37670c1789f82f01ee03352c972b5d0dfdac7
Patrick Carlisle <patrick@puppetlabs.com>
2014-03-12 13:38:42 -0700

    Prepare for 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,15 @@
-(defproject puppetlabs/http-client "0.1.0-SNAPSHOT"
+(defproject puppetlabs/http-client "0.1.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [puppetlabs/kitchensink "0.5.2"]
-                 [http-kit "2.1.16"]])
+                 [http-kit "2.1.16"]
+                 [puppetlabs/kitchensink "0.5.2"]]
+
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :username :env/clojars_jenkins_username
+                                     :password :env/clojars_jenkins_password
+                                     :sign-releases false}]]
+
+  :lein-release {:scm :git
+                 :deploy-via :lein-reploy}
+
+  :plugins [[lein-release "1.0.5"]]
+  )
