79631d28bddcd3a2b4a079d2150e3486bfc03d8d
Patrick Carlisle <patrick@puppetlabs.com>
2014-03-11 17:19:37 -0700

    Make SSL configuration more flexible and refactor
    This makes it possible to pass in an SSLEngine, SSLContext or set of PEM
    files. It also significantly refactors the wrapper and leaves the default
    methods async.

--- src/puppetlabs/http/client.clj
+++ src/puppetlabs/http/client.clj
@@ -1,100 +1,101 @@
 ;; This namespace is a thin wrapper around the http client functionality provided
-;; by org.httpkit.client.  The wrapper currently serves two purposes:
+;; by org.httpkit.client. It makes SSL configuration more flexible, and allows
+;; the use of PEM files.
 ;;
-;; 1. Support simple configuration options (:ssl-cert, :ssl-key, :ssl-ca-cert)
-;;    for issuing HTTPS requests using PEM files on disk for SSL configuration
-;; 2. `request` wrapper function makes all requests appear to be synchronous,
-;;    for an API that is more compatible with the clj-http synchronous API that
-;;    we've been using elsewhere.
-;;
-;; Would like to expose a `request-async` function soon, to allow callers to
-;; use async functionality if they prefer.
+;; In the options to any request method, an existing SSLContext object can be
+;; supplied under :ssl-context. If this is present it will be used. If it's
+;; not, the wrapper will attempt to use a set of PEM files stored in
+;; (:ssl-cert :ssl-key :ssl-ca-cert) to create the SSLContext. It is also
+;; still possible to use an SSLEngine directly, and if this is present under
+;; the key :sslengine it will be used before any other options are tried.
+;; 
+;; See the puppetlabs.http.sync namespace for synchronous versions of all
+;; these methods.
+
 (ns puppetlabs.http.client
   (:require [org.httpkit.client :as http]
             [puppetlabs.kitchensink.ssl :as ssl])
   (:refer-clojure :exclude (get)))
 
-(defn initialize-ssl
+;; SSL configuration functions
+
+(defn- initialize-ssl-context-from-pems
+  [req]
+  (-> req
+    (assoc :ssl-context (ssl/pems->ssl-context
+                          (:ssl-cert req)
+                          (:ssl-key req)
+                          (:ssl-ca-cert req)))
+    (dissoc :ssl-cert :ssl-key :ssl-ca-cert)))
+
+(defn- configure-ssl-from-context
+  "Configures an SSLEngine in the request starting from an SSLContext"
   [req]
   (-> req
     (assoc :sslengine (.createSSLEngine (:ssl-context req)))
     (dissoc :ssl-context)))
 
-(defn configure-ssl
-  [req]
-  (if-not (every? #(req %) #{:ssl-cert :ssl-key :ssl-ca-cert})
-    req
-    (let [ssl-context (ssl/pems->ssl-context
-                        (:ssl-cert req)
-                        (:ssl-key req)
-                        (:ssl-ca-cert req))]
-      (-> req
-        (assoc :ssl-context ssl-context)
-        (initialize-sslengine)
-        (dissoc :ssl-cert :ssl-key :ssl-ca-cert)))))
-
-(defn configure-output
-  [req]
-  (if (:as req)
-    req
-    (assoc req :as :text)))
-
-(defn configure-req
+(defn- configure-ssl-from-pems
+  "Configures an SSLEngine in the request starting from a set of PEM files"
   [req]
   (-> req
-    configure-ssl
-    configure-output))
+    initialize-ssl-context-from-pems
+    configure-ssl-from-context))
 
-(defn request
+(defn configure-ssl
+  "Configures a request map to have an SSLEngine. It will use an existing one
+  if already present, , then use an SSLContext (stored in :ssl-context) if
+  that is present, and will fall back to a set of PEM files (stored in
+  :ssl-cert, :ssl-key, and :ssl-ca-cert) if those are present. If none of
+  these are present this does not modify the request map."
   [req]
-  (let [{:keys [status headers body error] :as resp}
-          @(http/request (configure-req req) nil)]
-    (if error
-      (throw error)
-      resp)))
+  (cond
+    (:sslengine req) req
+    (:ssl-context req) (configure-ssl-from-context req)
+    (every? (partial req) [:ssl-cert :ssl-key :ssl-ca-cert]) (configure-ssl-from-pems req)
+    :else req))
 
-(defn check-url! [url]
+(defn- check-url! [url]
   (when (nil? url)
      (throw (IllegalArgumentException. "Host URL cannot be nil"))))
 
-(defn get
-  "Like #'request, but sets the :method and :url as appropriate."
-  [url & [req]]
-  (check-url! url)
-  (request (merge req {:method :get :url url})))
+(defn request
+  [opts callback]
+  (http/request (configure-ssl opts callback)))
 
-(defn head
-  "Like #'request, but sets the :method and :url as appropriate."
-  [url & [req]]
-  (check-url! url)
-  (request (merge req {:method :head :url url})))
+(defn- wrap-with-ssl-config
+  [method]
+  (fn wrapped-fn
+    ([url] (wrapped-fn url nil {}))
+    ([url callback] (wrapped-fn url callback {}))
+    ([url callback opts]
+     (check-url! url)
+     (method url (configure-ssl opts) callback))))
 
-(defn post
-  "Like #'request, but sets the :method and :url as appropriate."
-  [url & [req]]
-  (check-url! url)
-  (request (merge req {:method :post :url url})))
+(def ^{:arglists '([url] [url callback] [url callback opts])} get
+  "Issue an async HTTP GET request."
+  (wrap-with-ssl-config http/get))
 
-(defn put
-  "Like #'request, but sets the :method and :url as appropriate."
-  [url & [req]]
-  (check-url! url)
-  (request (merge req {:method :put :url url})))
+(def ^{:arglists '([url] [url callback] [url callback opts])} head
+  "Issue an async HTTP HEAD request."
+  (wrap-with-ssl-config http/head))
 
-(defn delete
-  "Like #'request, but sets the :method and :url as appropriate."
-  [url & [req]]
-  (check-url! url)
-  (request (merge req {:method :delete :url url})))
+(def ^{:arglists '([url] [url callback] [url callback opts])} post
+  "Issue an async HTTP POST request."
+  (wrap-with-ssl-config http/post))
 
-(defn options
-  "Like #'request, but sets the :method and :url as appropriate."
-  [url & [req]]
-  (check-url! url)
-  (request (merge req {:method :options :url url})))
+(def ^{:arglists '([url] [url callback] [url callback opts])} put
+  "Issue an async HTTP PUT request."
+  (wrap-with-ssl-config http/put))
 
-(defn patch
-  "Like #'request, but sets the :method and :url as appropriate."
-  [url & [req]]
-  (check-url! url)
-  (request (merge req {:method :patch :url url})))
+(def ^{:arglists '([url] [url callback] [url callback opts])} delete
+  "Issue an async HTTP DELETE request."
+  (wrap-with-ssl-config http/delete))
+
+(def ^{:arglists '([url] [url callback] [url callback opts])} options
+  "Issue an async HTTP OPTIONS request."
+  (wrap-with-ssl-config http/options))
+
+(def ^{:arglists '([url] [url callback] [url callback opts])} patch
+  "Issue an async HTTP PATCH request."
+  (wrap-with-ssl-config http/patch))
--- test/puppetlabs/http/client_test.clj
+++ test/puppetlabs/http/client_test.clj
@@ -1,6 +1,7 @@
 (ns puppetlabs.http.client-test
   (:require [clojure.test :refer :all]
             [clojure.java.io :refer [resource]] 
+            [puppetlabs.kitchensink.ssl :as ks-ssl]
             [puppetlabs.http.client :as http])
   (:import [javax.net.ssl SSLEngine]))
 
@@ -20,10 +21,33 @@
       (is (not (:ssl-key configured-req)))
       (is (not (:ssl-ca-cert configured-req))))))
 
-(deftest ssl-config-without-files
+(deftest ssl-config-without-ssl-params
   (let [req {:url "http://localhost"
              :method :get}
         configured-req (http/configure-ssl req)]
 
-    (testing "configure-ssl does nothing when given no files"
+    (testing "configure-ssl does nothing when given no ssl parameters"
+      (is (= req configured-req)))))
+
+(deftest ssl-config-with-context
+  (let [req {:url "http://localhost"
+             :method :get
+             :ssl-context (ks-ssl/pems->ssl-context
+                            (resource "resources/cert.pem")
+                            (resource "resources/key.pem")
+                            (resource "resources/ca.pem"))}
+        configured-req (http/configure-ssl req)]
+
+    (testing "configure-ssl uses an existing ssl context"
+      (is (instance? SSLEngine (:sslengine configured-req))))))
+
+(deftest ssl-config-with-sslengine
+  (let [req {:url "http://localhost"
+             :method :get
+             :ssl-cert (resource "resources/cert.pem")
+             :ssl-key (resource "resources/key.pem")
+             :ssl-ca-cert (resource "resources/ca.pem")
+             :sslengine "thing"}
+        configured-req (http/configure-ssl req)]
+    (testing "configure-ssl does nothing when :sslengine is given"
       (is (= req configured-req)))))
