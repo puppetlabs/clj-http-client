5ac20103be20fcca3171705356ff57cf7b58f8e0
Patrick Carlisle <patrick@puppetlabs.com>
2014-03-12 13:45:29 -0700

    Fix typo in project.clj and add some metadata

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject puppetlabs/http-client "0.1.0"
+  :description "HTTP client wrapper"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [http-kit "2.1.16"]
                  [puppetlabs/kitchensink "0.5.2"]]
@@ -9,7 +13,6 @@
                                      :sign-releases false}]]
 
   :lein-release {:scm :git
-                 :deploy-via :lein-reploy}
+                 :deploy-via :lein-deploy}
 
-  :plugins [[lein-release "1.0.5"]]
-  )
+  :plugins [[lein-release "1.0.5"]])
