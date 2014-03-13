d2eb421214002d1524478daa1428fdb2497a031b
Jenkins User <jenkins@debian-7-x64.acctest.dc1.puppetlabs.net>
2014-03-13 11:20:54 -0700

    lein-release plugin: preparing 0.1.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/http-client "0.1.2-SNAPSHOT"
+(defproject puppetlabs/http-client "0.1.2"
   :description "HTTP client wrapper"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
