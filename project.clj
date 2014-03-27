a90b6fd82c725fdbda6b4733902f0dfa65b7c6e3
Chris Price <chris@puppetlabs.com>
2014-03-27 12:46:44 -0700

    Move clojure source into clj directory

--- project.clj
+++ project.clj
@@ -7,6 +7,9 @@
                  [http-kit "2.1.16"]
                  [puppetlabs/kitchensink "0.5.2"]]
 
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/java"]
+
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :username :env/clojars_jenkins_username
                                      :password :env/clojars_jenkins_password
