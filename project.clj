76722c2e4a267ae57854515a0afb5e53fa5babd2
Chris Price <chris@puppetlabs.com>
2014-04-25 14:55:31 -0700

    (PE-3769) Build source jars and use explicit JDK target version

--- project.clj
+++ project.clj
@@ -6,6 +6,11 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
 
+  ;; Abort when version ranges or version conflicts are detected in
+  ;; dependencies. Also supports :warn to simply emit warnings.
+  ;; requires lein 2.2.0+.
+  :pedantic? :abort
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [http-kit "2.1.16"]
                  [puppetlabs/certificate-authority "0.1.4"]
@@ -14,11 +19,21 @@
 
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
+  :jar-exclusions [#".*\.java$"]
+  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
+
+  ;; By declaring a classifier here and a corresponding profile below we'll get an additional jar
+  ;; during `lein jar` that has all the source code (including the java source). Downstream projects can then
+  ;; depend on this source jar using a :classifier in their :dependencies.
+  :classifiers [["sources" :sources-jar]]
 
   :profiles {:dev {:dependencies [[puppetlabs/kitchensink ~ks-version :classifier "test"]
                                   [puppetlabs/trapperkeeper ~tk-version]
                                   [puppetlabs/trapperkeeper ~tk-version :classifier "test"]
-                                  [puppetlabs/trapperkeeper-webserver-jetty9 "0.5.0"]]}}
+                                  [puppetlabs/trapperkeeper-webserver-jetty9 "0.5.0"]]}
+             :sources-jar {:java-source-paths ^:replace []
+                           :jar-exclusions ^:replace []
+                           :source-paths ^:replace ["src/clojure" "src/java"]}}
 
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :username :env/clojars_jenkins_username
