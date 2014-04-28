f6c4b5b059801063ee96355f4ffb083cb0339a45
Jenkins User <jenkins@debian-7-x64.acctest.dc1.puppetlabs.net>
2014-04-28 11:48:54 -0700

    lein-release plugin: bumped version from 0.1.7 to 0.1.8-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (def ks-version "0.6.0")
 (def tk-version "0.3.10")
 
-(defproject puppetlabs/http-client "0.1.7"
+(defproject puppetlabs/http-client "0.1.8-SNAPSHOT"
   :description "HTTP client wrapper"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
