cff90fbea2984aec0a40698aedd78dd539d1d638
Jenkins CI <ci@puppetlabs.com>
2017-02-09 14:30:16 -0800

    Version 0.8.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/http-client "0.8.0"
+(defproject puppetlabs/http-client "0.8.1-SNAPSHOT"
   :description "HTTP client wrapper"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
