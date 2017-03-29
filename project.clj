0c207527d50c474fedd686a6d11f80b5da8f51f9
Matthaus Owens <matthaus@puppet.com>
2017-03-29 09:22:49 -0700

    (PE-19979) Update clj-parent to 0.6.1 and i18n to 0.8.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
 
   :min-lein-version "2.7.1"
 
-  :parent-project {:coords [puppetlabs/clj-parent "0.4.3"]
+  :parent-project {:coords [puppetlabs/clj-parent "0.6.1"]
                    :inherit  [:managed-dependencies]}
 
   ;; Abort when version ranges or version conflicts are detected in
@@ -54,4 +54,4 @@
                  :deploy-via :lein-deploy}
 
   :plugins [[lein-parent "0.3.1"]
-            [puppetlabs/i18n "0.7.1"]])
+            [puppetlabs/i18n "0.8.0"]])
